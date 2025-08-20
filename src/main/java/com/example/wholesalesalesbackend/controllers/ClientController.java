6745e48f8b4ea8670fd3b6b1f54d18c2eb8d29b2
tusharjain8465 <tusharjain8465@gmail.com>
2025-08-20 16:52:58 +0530

    Update ClientController.java

--- src/main/java/com/example/wholesalesalesbackend/controllers/ClientController.java
+++ src/main/java/com/example/wholesalesalesbackend/controllers/ClientController.java
@@ -21,7 +21,7 @@ import com.example.wholesalesalesbackend.service.ClientService;
 import jakarta.validation.Valid;
 
 @RestController
-@CrossOrigin(origins = "https://salesmanager-phi.vercel.app")
+@CrossOrigin(origins = "front-end-vercel-azure.vercel.app")
 @RequestMapping("/api/clients")
 public class ClientController {
 
